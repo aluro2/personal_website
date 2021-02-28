be1de947dfbbe31468a589b6e6f11809f3d84f4a
Alec Luro <alec.b.luro@gmail.com>
2021-02-28 16:40:51 -0600

    add link to usgs bbs app

--- docker_container.sh
+++ docker_container.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "Building RStudio container for website build"
 
 docker run --rm -d -p 8787:8787 -v $(pwd):/home/rstudio -e DISABLE_AUTH=true --name personal-website-build aluro2/rstudio-blogdown-personal-website:v1
 
+echo "Container ready! Go to http://localhost:8787/"
